332d0e52f7367af5eaf36a991051f613665b041c
unknown <onuchevictoratabo2020@gmail.com>
2024-02-17 19:22:33 +0100

    updated

--- script.js
+++ script.js
@@ -87,3 +87,26 @@ const mainNavObserver = new IntersectionObserver(stickyNav, {
 });
 
 mainNavObserver.observe(mainNav);
+
+// TUTORIAL #198 (REVEALLING ELEMENTS ON SROLL)
+
+const allSection = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  // section.classList.add("section--hidden");
+});
